7bd68a6571e297f7feee0fbedf9059b606ca7c56
arlofeirman <arlofeirman@gmail.com>
2017-06-23 15:24:01 -0400

    Add scope to conversation model
    Adds scope to app/models/conversation.rb. To prevent more than one
    conversation from being created between an engineer and a recruiter.
    
    Next steps: generate migration to drops unnecessary columns from
    conversations table.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,9 @@ class Conversation < ApplicationRecord
   belongs_to :engineer
   belongs_to :recruiter
   validates_uniqueness_of :engineer_id, scope: :recruiter_id
+
+  scope :between, -> (sender_id,recipient_id) do
+      where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
+    end
+
 end
