398be7a08e56c8927c4422b5c31d9a2571abcda9
arlofeirman <arlofeirman@gmail.com>
2017-06-23 16:27:22 -0400

    Run migration to create messages table

--- db/migrate/20170623195235_create_messages.rb
+++ db/migrate/20170623195235_create_messages.rb
@@ -1,9 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.text :body
-      t.references :conversation, index: true
-      t.references :lunchable, polymorphic: true, index: true
+      t.text :body, null: false
+      t.references :conversation, index: true, null: false
+      t.references :lunchable, polymorphic: true, index: true, null: false
       t.boolean :read, default: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623193003) do
+ActiveRecord::Schema.define(version: 20170623195235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 20170623193003) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_engineers_on_user_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body",                            null: false
+    t.integer  "conversation_id",                 null: false
+    t.string   "lunchable_type",                  null: false
+    t.integer  "lunchable_id",                    null: false
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["lunchable_type", "lunchable_id"], name: "index_messages_on_lunchable_type_and_lunchable_id", using: :btree
   end
 
   create_table "recruiters", force: :cascade do |t|
