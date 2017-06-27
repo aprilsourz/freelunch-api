5e0995183a6dcec396bff76944a6bb78cd9c566a
arlofeirman <arlofeirman@gmail.com>
2017-06-27 13:13:55 -0400

    Add lunchable_id and lunchable_type to serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,4 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :body, :conversation_id, :sender_name, :created_at, :read
+  attributes :id, :body, :conversation_id, :sender_name, :created_at, :read,
+             :lunchable_id, :lunchable_type
 end
